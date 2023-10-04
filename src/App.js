f53de35d2f211085e3ff54314d521a3198a6b3d5
RyoTonegawa <tonegawa622@gmail.com>
2023-10-05 01:34:26 +0900

    git change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';  // ReactおよびuseStateフックをインポート
+import axios from 'axios';  // APIリクエストのためのaxiosライブラリをインポート
+import './App.css';  // スタイルシートをインポート
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // ステート変数の初期化
+    const [input, setInput] = useState('');  // 入力テキスト
+    const [chatHistory, setChatHistory] = useState([]);  // チャットの履歴
+
+    // 送信ボタンの処理
+    const handleSend = async () => {
+        if(input) {
+            // 入力をチャット履歴に追加
+            setChatHistory([...chatHistory, { type: 'user', text: input }]);
+            try {
+                // APIリクエストを送信してレスポンスを受け取る
+                const response = await axios.post('http://localhost:5000/predict', { text: input });
+                // ボットの返答をチャット履歴に追加
+                setChatHistory([...chatHistory, { type: 'user', text: input }, { type: 'bot', text: response.data.result }]);
+            } catch(error) {
+                console.error("Error:", error);  // エラー時の処理
+            }
+            setInput('');  // 入力フィールドをクリア
+        }
+    }
+
+    // チャットのUIを表示
+    return (
+        <div className="App">
+            <div className="chatWindow">
+                {chatHistory.map((chat, index) => (
+                    <div key={index} className={`chatBubble ${chat.type}`}>{chat.text}</div>
+                ))}
+            </div>
+            <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+            <button onClick={handleSend}>Send</button>
+        </div>
+    );
 }
 
 export default App;
